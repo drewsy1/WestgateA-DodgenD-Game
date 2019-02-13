f5d5756f3563dff2a0b5ed061067163bf5dc8eb4
Drew Westgate <drew.westgate@outlook.com>
2019-02-13 14:48:41 -0600

    Refactored/rewrote Projectile.vb to place most logic/code in the base class.

--- WestgateA-DodgenD-Game/Classes/Projectile.vb
+++ WestgateA-DodgenD-Game/Classes/Projectile.vb
@@ -1,65 +1,78 @@
-﻿Imports WestgateA_DodgenD_Game
+﻿Namespace Classes
+    Public MustInherit Class Projectile
+        Private Shared ReadOnly MainWindowInstance As MainWindow = Application.Current.MainWindow 'Get the MainWindow as an object
+        Friend Shared ReadOnly ProjectilesCollection As List(Of Projectile) = New List(Of Projectile)() ' Create a static collection of projectiles to keep track of them
+        Private Const ProjectileHeight As Double = 27 ' Default projectile height
+        Private Const ProjectileWidth As Double = 3 ' Default projectile width
+        Protected Overridable Property ProjectileDirection As Double ' Overridable double representing direction of projectile travel
 
-Namespace Classes
-    Public Class Projectile
-        Friend Shared ReadOnly ProjectilesCollection As List(Of Projectile) = New List(Of Projectile)()
-        Private Const ProjectileHeight As Double = 27
-        Private Const ProjectileWidth As Double = 3
-        Private Const ProjectileDirection As Double = -1
-        Friend ReadOnly _ProjectileTransformTranslate As TranslateTransform = New TranslateTransform() With {.X = 0, .Y = 0}
-        Friend ReadOnly _ProjectileTransform As TransformGroup = New TransformGroup()
-        Friend ProjectileRectangle As Rectangle = New Rectangle() With {
-            .Height = ProjectileHeight,
-            .Width = ProjectileWidth,
-            .StrokeThickness = 0
+        Private ReadOnly _projectileTransformTranslate As TranslateTransform = New TranslateTransform() With {.X = 0, .Y = 0} ' Transform object that will be added to the projectile. This object's X and Y properties will move the projectile
+
+        ' TransformGroup containing _projectileTransformTranslate, will be added to ProjectileRectangle
+        Private ReadOnly _projectileTransform As TransformGroup = New TransformGroup() With {
+            .Children = New TransformCollection(New Transform() {_projectileTransformTranslate})
         }
 
-        Sub New()
+        ' Rectangle object that serves as projectile
+        Private ReadOnly _projectileRectangle As Rectangle = New Rectangle() With {
+            .Height = ProjectileHeight,
+            .Width = ProjectileWidth,
+            .StrokeThickness = 0,
+            .RenderTransform = _projectileTransform
+        }
 
+        Protected Sub New()
+            ProjectilesCollection.Add(Me)
+        End Sub
+
+        Protected Sub SetColor(projectileColor As Color)
+            _projectileRectangle.Fill = New SolidColorBrush(projectileColor)
         End Sub
 
         Sub UpdateLocation()
-            _ProjectileTransformTranslate.Y += (10 * ProjectileDirection)
+            _projectileTransformTranslate.Y += (10 * ProjectileDirection)
+            'If
+        End Sub
+
+        Protected Overridable Sub AddToCanvas(location As Double, transformLeft As Double, transformTop As Double)
+            Canvas.SetLeft(_projectileRectangle, transformLeft)
+            Canvas.SetTop(_projectileRectangle, transformTop)
+            _projectileTransformTranslate.X += location
+            MainWindowInstance.CanvasGameScreen.Children.Add(_projectileRectangle)
         End Sub
     End Class
 
     Public Class ProjectilePlayer
         Inherits Projectile
-        Private Const ProjectileDirection As Double = -1
+        Protected Overrides Property ProjectileDirection As Double = -1
+        Shared Shadows ReadOnly ProjectileColor As Color = Colors.LawnGreen
+        Private Const TransformLeftDefault As Double = 316.5
+        Private Const TransformTopDefault As Double = 636
 
         Sub New()
-            _ProjectileTransform.Children.Add(_ProjectileTransformTranslate)
-            ProjectileRectangle.Fill = New SolidColorBrush(Colors.LawnGreen)
-            ProjectileRectangle.RenderTransform = _ProjectileTransform
-            ProjectilesCollection.Add(Me)
+            MyBase.New()
+            SetColor(ProjectileColor)
         End Sub
 
-        Sub AddToCanvas(location As Double)
-            Dim mw As MainWindow = Application.Current.MainWindow
-            Canvas.SetLeft(ProjectileRectangle, 316.5)
-            Canvas.SetTop(ProjectileRectangle, 636)
-            _ProjectileTransformTranslate.X += location
-            mw.CanvasGameScreen.Children.Add(ProjectileRectangle)
+        Shadows Sub AddToCanvas(location As Double, Optional transformLeft As Double = TransformLeftDefault, Optional transformTop As Double = TransformTopDefault)
+            MyBase.AddToCanvas(location, transformLeft, transformTop)
         End Sub
     End Class
 
     Public Class ProjectileEnemy
         Inherits Projectile
-        Private Const ProjectileDirection As Double = 1
+        Protected Overrides Property ProjectileDirection As Double = 1
+        Shared Shadows ReadOnly ProjectileColor As Color = Colors.Red
+        Private Const TransformLeftDefault As Double = 316.5
+        Private Const TransformTopDefault As Double = 0
 
         Sub New()
-            _ProjectileTransform.Children.Add(_ProjectileTransformTranslate)
-            ProjectileRectangle.Fill = New SolidColorBrush(Colors.Red)
-            ProjectileRectangle.RenderTransform = _ProjectileTransform
-            ProjectilesCollection.Add(Me)
+            MyBase.New()
+            SetColor(ProjectileColor)
         End Sub
 
-        Sub AddToCanvas(location As Double)
-            Dim mw As MainWindow = Application.Current.MainWindow
-            Canvas.SetLeft(ProjectileRectangle, 316.5)
-            Canvas.SetTop(ProjectileRectangle, 100)
-            _ProjectileTransformTranslate.X += location
-            mw.CanvasGameScreen.Children.Add(ProjectileRectangle)
+        Shadows Sub AddToCanvas(location As Double, Optional transformLeft As Double = TransformLeftDefault, Optional transformTop As Double = TransformTopDefault)
+            MyBase.AddToCanvas(location, transformLeft, transformTop)
         End Sub
     End Class
 End Namespace
