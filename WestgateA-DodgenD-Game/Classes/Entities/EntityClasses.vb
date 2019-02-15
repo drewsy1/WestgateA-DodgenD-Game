b13ee72a2af743c852f7e8c8649397af96884550
Drew Westgate <drew.westgate@outlook.com>
2019-02-15 01:41:43 -0600

    Abstracted most of Player into separate Entity classes to allow for easier creation of future entities (such as enemies)

---
+++ WestgateA-DodgenD-Game/Classes/Entities/EntityClasses.vb
@@ -0,0 +1,13 @@
+﻿Namespace Classes.Entities
+    ' ReSharper disable once ClassNeverInstantiated.Global
+    ''' <summary>
+    ''' Class containing Projectile classes and meta-properties
+    ''' </summary>
+    Partial Public Class EntityClasses
+        ''' <summary>
+        ''' Create a static collection of projectiles to keep track of them
+        ''' </summary>
+        Private Shared ReadOnly EntitiesCollection As List(Of EntityBase) =
+                               New List(Of EntityBase)()
+    End Class
+End Namespace
