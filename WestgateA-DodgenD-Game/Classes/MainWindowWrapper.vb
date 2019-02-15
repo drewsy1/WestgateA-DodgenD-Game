c70e80ac89caf92cc169c2700ab36a067d8fe3bf
Andrew Westgate <aew027@ad3.latech.edu>
2019-02-15 01:49:00 -0600

    Added distinct, fixed CanvasWidth and CanvasHeight shared properties to MainWindowWrapper

--- WestgateA-DodgenD-Game/Classes/MainWindowWrapper.vb
+++ WestgateA-DodgenD-Game/Classes/MainWindowWrapper.vb
@@ -4,7 +4,16 @@
         ''' <summary>
         ''' Gets the MainWindow as an object and provides it as a static object
         ''' </summary>
-        Public Shared ReadOnly MainWindowInstance As MainWindow =
-                                   Application.Current.MainWindow
+        Public Shared ReadOnly MainWindowInstance As MainWindow = Application.Current.MainWindow
+
+        ''' <summary>
+        ''' Width of canvas in pixels
+        ''' </summary>
+        Public Const CanvasWidth As Double = 672
+
+        ''' <summary>
+        ''' Height of canvas in pixels
+        ''' </summary>
+        Public Const CanvasHeight As Double = 768
     End Class
 End Namespace
