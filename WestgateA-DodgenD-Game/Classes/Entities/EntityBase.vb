cc1872ed04ff27d0d3760cafb79859c512846e23
Drew Westgate <drew.westgate@outlook.com>
2019-02-14 23:13:17 -0600

    Created new EntityBase class

---
+++ WestgateA-DodgenD-Game/Classes/Entities/EntityBase.vb
@@ -0,0 +1,12 @@
+﻿Namespace Classes.Entities
+    Public MustInherit Class EntityBase
+        Protected Overridable Property EntityHeight As Double
+
+        Protected Overridable Property EntityWidth As Double
+
+        Protected Overridable Property LocationXDefault As Double
+
+        Protected Overridable Property LocationYDefault As Double
+
+    End Class
+End Namespace
