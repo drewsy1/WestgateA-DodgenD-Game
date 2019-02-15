9f688fac5dfb6ebf577a7d9385d2d3d1b7329f6e
Andrew Westgate <aew027@ad3.latech.edu>
2019-02-14 21:36:11 -0600

    Created GameTimer class to handle program-wide timer functions

---
+++ WestgateA-DodgenD-Game/Classes/GameTimer.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Windows.Threading
+
+Namespace Classes
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    Public Class GameTimer
+        ''' <summary>
+        ''' Timer that keeps track of game events/objects
+        ''' </summary>
+        Private Shared WithEvents _dtTimer As DispatcherTimer = New DispatcherTimer() With {
+            .Interval = TimeSpan.FromMilliseconds(1)
+        }
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        Public Shared Event Tick()
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        Public Shared Sub Start()
+            _dtTimer.Start()
+        End Sub
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        Private Shared Sub _RaiseEvent() Handles _dtTimer.Tick
+            RaiseEvent Tick()
+        End Sub
+
+    End Class
+End Namespace
