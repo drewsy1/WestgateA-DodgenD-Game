7386e6aa4b258ba51a78503f4fbbaba84cf90f90
Drew Westgate <drew.westgate@outlook.com>
2019-02-16 16:08:00 -0600

    Created EnemyB

--- WestgateA-DodgenD-Game/Classes/Entities/EntityEnemy_B.vb
+++ WestgateA-DodgenD-Game/Classes/Entities/EntityEnemy_B.vb
@@ -5,29 +5,30 @@ Namespace Classes.Entities
         Public Class EntityEnemyB
             Inherits EntityEnemy
 
-            Private Shared ReadOnly EnemyACollection As List(Of Object) =
+            Private Shared ReadOnly EnemyBCollection As List(Of Object) =
                                           New List(Of Object)()
 
-            Public Overrides Property ObjectControl As Object = New Button() With {
-                                .Name = "EnemyA_" & EnemyACollection.Count,
+            Public Overrides Property ObjectControl As Object = New TextBox() With {
+                                .Name = "EnemyB_" & EnemyBCollection.Count,
                                 .Height = ObjectHeight,
                                 .Width = ObjectWidth,
                                 .RenderTransform = ObjectTransformGroup,
                                 .RenderTransformOrigin = New Point(0, 0),
-                                .Content = "😠",
+                                .Text = "☢",
                                 .FontFamily = New FontFamily("Segoe UI Symbol"),
-                                .FontSize = 20,
+                                .FontSize = 36,
                                 .VerticalContentAlignment = VerticalAlignment.Center,
-                                .Padding = New Thickness(0),
-                                .IsEnabled = False,
-                                .FontWeight = FontWeights.UltraBold
+                                .Padding = New Thickness(-2.5, -11.5, 0, 0),
+                                .IsReadOnly = True,
+                                .Background = New SolidColorBrush(Color.FromRgb(255, 255, 0)),
+                                .Foreground = New SolidColorBrush(Color.FromRgb(0, 0, 0))
                 }
 
             Public Shadows WithEvents ObjectHitbox As Hitbox
 
             Sub New()
                 MyBase.New()
-                EnemyACollection.Add(Me)
+                EnemyBCollection.Add(Me)
 
                 MainWindowWrapper.SetCanvasLocation(
                     LocationXDefault,
@@ -40,6 +41,13 @@ Namespace Classes.Entities
 
             Sub ChangeContent()
                 'Fires every half-second
+                If ObjectControl.Background.Color = Color.FromRgb(255, 255, 0) Then
+                    ObjectControl.Background.Color = Color.FromRgb(0, 0, 0)
+                    ObjectControl.Foreground.Color = Color.FromRgb(255, 255, 0)
+                Else
+                    ObjectControl.Background.Color = Color.FromRgb(255, 255, 0)
+                    ObjectControl.Foreground.Color = Color.FromRgb(0, 0, 0)
+                End If
             End Sub
         End Class
     End Class
