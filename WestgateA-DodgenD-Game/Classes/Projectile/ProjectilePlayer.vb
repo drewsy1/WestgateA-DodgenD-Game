2c85468397fd3ec1440602e83fe5ed0ead83d2bf
Drew Westgate <drew.westgate@outlook.com>
2019-02-20 02:10:44 -0600

    Reformatted/cleaned ProjectilePlayer

--- WestgateA-DodgenD-Game/Classes/Projectile/ProjectilePlayer.vb
+++ WestgateA-DodgenD-Game/Classes/Projectile/ProjectilePlayer.vb
@@ -1,31 +1,15 @@
-﻿Imports WestgateA_DodgenD_Game.Classes.Entities
-Imports WestgateA_DodgenD_Game.Enums
+﻿Imports WestgateA_DodgenD_Game.Enums
 
 Namespace Classes.Projectile
+
     ' ReSharper disable once ClassNeverInstantiated.Global
     Partial Public Class ProjectileClasses
+
         ''' <summary>
         ''' Defines properties/methods of a player-fired projectile
         ''' </summary>
         Public Class ProjectilePlayer
             Inherits ProjectileBase
-
-            ''' <summary>
-            ''' Direction of projectile
-            ''' </summary>
-            ''' <returns>ProjectileDirection</returns>
-            Protected Overrides ReadOnly Property ProjectileDirection As Double = EProjectileDirection.Up
-
-            ''' <summary>
-            ''' Color of ProjectilePlayer projectile
-            ''' </summary>
-            Protected Overrides Property ProjectileColor As Color = Colors.LawnGreen
-
-            Protected Overrides ReadOnly Property LocationCoordsDefault As Point
-                Get
-                    Return New Point(Application.CanvasWidth / 2, Application.CanvasHeight)
-                End Get
-            End Property
 
             ''' <summary>
             ''' Instantiates a ProjectilePlayer object by calling Projectile.New() and
@@ -43,11 +27,30 @@ Namespace Classes.Projectile
                 AddHandler Application.EnemyHit, AddressOf Remove
             End Sub
 
+            Protected Overrides ReadOnly Property LocationCoordsDefault As Point
+                Get
+                    Return New Point(Application.CanvasWidth / 2, Application.CanvasHeight)
+                End Get
+            End Property
+
+            ''' <summary>
+            ''' Color of ProjectilePlayer projectile
+            ''' </summary>
+            Protected Overrides Property ProjectileColor As Color = Colors.LawnGreen
+
+            ''' <summary>
+            ''' Direction of projectile
+            ''' </summary>
+            ''' <returns>ProjectileDirection</returns>
+            Protected Overrides ReadOnly Property ProjectileDirection As Double = EProjectileDirection.Up
             Overrides Sub Remove()
                 RemoveHandler Application.EnemyHit, AddressOf Remove
                 MyBase.Remove()
                 Application.RaisePlayerProjectileRemove(Me)
             End Sub
+
         End Class
+
     End Class
+
 End Namespace
