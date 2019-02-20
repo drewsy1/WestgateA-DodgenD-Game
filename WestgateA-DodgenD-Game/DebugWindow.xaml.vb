2fcf2dd0b487b948c99b0304d234ce139ec77552
Drew Westgate <drew.westgate@outlook.com>
2019-02-20 02:11:46 -0600

    Reformatted and added to DebugWindow

--- WestgateA-DodgenD-Game/DebugWindow.xaml.vb
+++ WestgateA-DodgenD-Game/DebugWindow.xaml.vb
@@ -1,42 +1,42 @@
 ﻿Imports System.Collections.ObjectModel
-Imports WestgateA_DodgenD_Game.Classes
+Imports System.Globalization
+Imports System.Windows.Markup
 Imports WestgateA_DodgenD_Game.Classes.Entities
+Imports WestgateA_DodgenD_Game.Classes.Projectile
 
 Public Class DebugWindow
-    Public Property CanvasWidth As Double = Application.CanvasWidth
-
-    Public Property PlayerEntity As EntityClasses.EntityPlayer = Application.EntityPlayerObject
-
-    Public Property PlayerProjectileValue As Double = PlayerEntity.ObjectTransformTranslate.Y
-
-    Public Property PlayerLeftBound As Double = PlayerEntity.LocationCoords.X + PlayerEntity.TranslateBoundLeft
-
-    Public Property PlayerRightBound As Double = PlayerEntity.LocationCoords.X + PlayerEntity.TranslateBoundRight
-
-    Public Property GameScore As Integer = Application.GameScore
-    Public Property GameLevel As Integer = Application.GameLevel
-    Public Property GameLives As Integer = Application.GameLives
-
-
-    Public Property EnemyCollection As ObservableCollection(Of EntityClasses.EntityEnemyBase) = Application.EnemyCollection
-
     Sub New()
         ' This call is required by the designer.
         InitializeComponent()
 
         AddHandler EntityClasses.EntityPlayer.PressFireButton, AddressOf PressFireButton
         AddHandler Application.ReleaseFireButton, AddressOf ReleaseFireButton
+        AddHandler Application.ProjectileHit, AddressOf RefreshDataGridEnemies
         ' Add any initialization after the InitializeComponent() call.
 
-
     End Sub
 
+    Public Property CanvasWidth As Double = Application.CanvasWidth
 
+    Public Property EnemyCollection As ObservableCollection(Of EntityClasses.EntityEnemyBase) = Application.EnemyCollection
+    Public Property GameLevel As Integer = Application.GameLevel
+    Public Property GameLives As Integer = Application.GameLives
+    Public Property GameScore As Integer = Application.GameScore
+    Public Property PlayerEntity As EntityClasses.EntityPlayer = Application.EntityPlayerObject
+
+    Public Property PlayerLeftBound As Double = PlayerEntity.LocationCoords.X + PlayerEntity.TranslateBoundLeft
+    Public Property PlayerProjectileValue As Double = PlayerEntity.ObjectTransform_Translate.Y
+    Public Property PlayerRightBound As Double = PlayerEntity.LocationCoords.X + PlayerEntity.TranslateBoundRight
     Public Sub PressFireButton()
-        textBoxFiring.Background = New SolidColorBrush(Color.FromRgb(255, 0, 0))
-    End Sub
-    Public Sub ReleaseFireButton()
-        textBoxFiring.Background = New SolidColorBrush(Color.FromRgb(255, 255, 255))
+        TextBoxFiring.Background = New SolidColorBrush(Color.FromRgb(255, 0, 0))
     End Sub
 
+    Public Sub ReleaseFireButton()
+        TextBoxFiring.Background = New SolidColorBrush(Color.FromRgb(255, 255, 255))
+    End Sub
+
+    Public Sub RefreshDataGridEnemies()
+        
+    End Sub
+    
 End Class
